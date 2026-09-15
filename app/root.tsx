a33689a793a936652806f7a9c3bb344693db84de
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2026-09-15 18:11:59 -0300

    style: update CSS variables and improve theme config + bootstrap

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import bootstrapHref from "bootstrap/dist/css/bootstrap.min.css?url";
 import {
 	isRouteErrorResponse,
 	Links,
@@ -21,6 +22,7 @@ export const links: Route.LinksFunction = () => [
 		rel: "stylesheet",
 		href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
 	},
+	{ rel: "stylesheet", href: bootstrapHref }, 
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -62,11 +64,11 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
 	}
 
 	return (
-		<main className="pt-16 p-4 container mx-auto">
+		<main className="container mx-auto p-4 pt-16">
 			<h1>{message}</h1>
 			<p>{details}</p>
 			{stack && (
-				<pre className="w-full p-4 overflow-x-auto">
+				<pre className="w-full overflow-x-auto p-4">
 					<code>{stack}</code>
 				</pre>
 			)}
